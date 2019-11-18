7ac2400b1781fbf42160d8b1031b41c389cd20e7
Chia Hou Tan <tanchiahou5309@hotmail.com>
2019-11-18 10:06:49 +0000

    commit change

--- app.js
+++ app.js
@@ -24,11 +24,3 @@ function fetchProducts() {
     });
 }
 
-ReactDOM.render(
-  React.createElement(
-    "h1",
-    {"style": {"color": "blue"}},
-    "Hello World"
-  ),
-  document.getElementById("root")
-);
---
+++ textREACT.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+    React.createElement(
+      "h1",
+      {"style": {"color": "blue"}},
+      "Hello World"
+    ),
+    document.getElementById("root")
+  );
