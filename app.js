23f3970e5c45def1a6eff1b04a2f5bd178f7df76
Chia Hou Tan <tanchiahou5309@hotmail.com>
2019-11-18 01:09:38 +0000

    commit go

--- app.js
+++ app.js
@@ -23,3 +23,12 @@ function fetchProducts() {
 
     });
 }
+
+ReactDOM.render(
+  React.createElement(
+    "h1",
+    {"style": {"color": "red"}},
+    "Hello World"
+  ),
+  document.getElementById("root")
+);
